98d1fd34a94be3a0ff9f719a51ab143484e7915b
app-muon <muonappdevelopment@gmail.com>
2025-09-05 12:07:36 +0100

    Remembers status on reopen now

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.taskfree.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
+        versionCode = 16
         versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
