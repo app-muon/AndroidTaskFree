721af6637c29290fb894ef110e0cdc0469e664fd
app-muon <muonappdevelopment@gmail.com>
2025-09-09 16:05:52 +0100

    Release with new colours

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.taskfree.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "1.0.12"
+        versionCode = 22
+        versionName = "1.0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
