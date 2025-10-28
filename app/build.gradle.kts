cf1063b92ded0921adbb55918971714c271488f4
app-muon <muonappdevelopment@gmail.com>
2025-10-28 21:48:33 +0000

    Reformatted task row

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.taskfree.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 41
-        versionName = "1.11"
+        versionCode = 42
+        versionName = "1.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
